085d91cffec95dfacf5d9f9bf6c789a8341a499b
InfiniteBlueGX <mpczarne@outlook.com>
2023-08-23 17:02:10 -0500

    Add a failsafe cooldown to prevent the process memory becoming too high (1GB for now)

--- WarcSearcher/WarcSearcher.py
+++ WarcSearcher/WarcSearcher.py
@@ -13,6 +13,7 @@ import zipfile
 from io import BytesIO
 from multiprocessing import Manager
 
+import psutil
 import py7zr
 import rarfile
 from fastwarc.stream_io import FileStream, GZipStream
@@ -81,6 +82,11 @@ def iterate_through_gz_files(gz_directory_path):
 
 
 def open_warc_gz_file(gz_file_path):
+    process = psutil.Process()
+    while get_total_memory_usage(process) > 1000000000:
+        log_warning(f"Process memory usage is above maximum. Will attempt to read the next WARC after 30 seconds to allow time to process the existing queue...")
+        time.sleep(30)
+
     gz_file_stream = GZipStream(FileStream(gz_file_path, 'rb'))
     logging.info(f"Beginning to process {gz_file_path}")
 
