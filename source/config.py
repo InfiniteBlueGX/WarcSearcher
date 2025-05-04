d3150f94f4be6c77bd1dee595c4c18dd137e6c27
InfiniteBlueGX <105176909+InfiniteBlueGX@users.noreply.github.com>
2025-05-04 10:35:54 -0500

    make validation more consistent

--- source/config.py
+++ source/config.py
@@ -14,8 +14,8 @@ settings = {
 
 
 def read_config_ini_variables():
-    """Reads the variables found in ther config.ini file after ensuring it exists."""
-    config_path = get_config_ini_path()
+    """Reads the variables found in ther config.ini file after ensuring the config.ini file exists."""
+    config_path = validate_and_get_config_ini_path()
 
     parser = configparser.ConfigParser()
     parser.read(config_path)
@@ -29,37 +29,26 @@ def read_config_ini_variables():
         sys.exit()
 
 
-def get_config_ini_path():
-    if os.path.isfile('config.ini'):
-        config_path = 'config.ini'
-    elif os.path.isfile('../config.ini'):
-        config_path = '../config.ini'
-    else:
-        log_error("config.ini file does not exist in the working directory or its parent.")
-        sys.exit()
-    return config_path
-
-
 def read_required_config_ini_variables(parser: configparser.ConfigParser):
     """Reads the required variables from the config.ini file, validates them, and sets them in the config dictionary."""
-    settings["WARC_GZ_ARCHIVES_DIRECTORY"] = parser.get('REQUIRED', 'WARC_GZ_ARCHIVES_DIRECTORY')
-    validate_warc_gz_archives_directory(settings["WARC_GZ_ARCHIVES_DIRECTORY"])
+    parsed_warc_gz_archives_directory = parser.get('REQUIRED', 'WARC_GZ_ARCHIVES_DIRECTORY')
+    settings["WARC_GZ_ARCHIVES_DIRECTORY"] = validate_and_get_warc_gz_archives_directory(parsed_warc_gz_archives_directory)
 
-    settings["SEARCH_REGEX_DEFINITIONS_DIRECTORY"] = parser.get('REQUIRED', 'SEARCH_REGEX_DEFINITIONS_DIRECTORY')
-    validate_search_regex_definitions_directory(settings["SEARCH_REGEX_DEFINITIONS_DIRECTORY"])
+    parsed_search_regex_definitions_directory = parser.get('REQUIRED', 'SEARCH_REGEX_DEFINITIONS_DIRECTORY')
+    settings["SEARCH_REGEX_DEFINITIONS_DIRECTORY"] = validate_and_get_search_regex_definitions_directory(parsed_search_regex_definitions_directory)
         
-    settings["RESULTS_OUTPUT_DIRECTORY"] = parser.get('REQUIRED', 'RESULTS_OUTPUT_DIRECTORY')
-    validate_results_output_directory(settings["RESULTS_OUTPUT_DIRECTORY"])
+    parsed_results_output_directory = parser.get('REQUIRED', 'RESULTS_OUTPUT_DIRECTORY')
+    settings["RESULTS_OUTPUT_DIRECTORY"] = validate_and_get_results_output_directory(parsed_results_output_directory)
 
 
 def read_optional_config_ini_variables(parser: configparser.ConfigParser):
     """Reads the optional variables from the config.ini file and sets them in the config settings dictionary."""
     settings["ZIP_FILES_WITH_MATCHES"] = parser.getboolean('OPTIONAL', 'ZIP_FILES_WITH_MATCHES')
 
-    parsed_max_concurrent_search_processes = parser.get('OPTIONAL', 'MAX_CONCURRENT_SEARCH_PROCESSES').lower()
+    parsed_max_concurrent_search_processes = parser.getint('OPTIONAL', 'MAX_CONCURRENT_SEARCH_PROCESSES')
     settings["MAX_CONCURRENT_SEARCH_PROCESSES"] = validate_and_get_max_concurrent_search_processes(parsed_max_concurrent_search_processes)
 
-    parsed_max_ram_useage_bytes = parser.get('OPTIONAL', 'MAX_RAM_USAGE_BYTES').lower()
-    settings["MAX_RAM_USAGE_BYTES"] = validate_and_get_max_ram_usage(parsed_max_ram_useage_bytes)
+    parsed_max_ram_useage_bytes = parser.getint('OPTIONAL', 'MAX_RAM_USAGE_BYTES')
+    settings["MAX_RAM_USAGE_BYTES"] = validate_and_get_max_ram_usage_bytes(parsed_max_ram_useage_bytes)
 
     settings["SEARCH_BINARY_FILES"] = parser.getboolean('OPTIONAL', 'SEARCH_BINARY_FILES')
--- source/validators.py
+++ source/validators.py
@@ -6,36 +6,54 @@ from logger import *
 from utilities import get_total_ram_bytes_rounded
 
 
-def validate_warc_gz_archives_directory(warc_gz_archives_directory: str):
+def validate_and_get_config_ini_path():
+    if os.path.isfile('config.ini'):
+        config_path = 'config.ini'
+    elif os.path.isfile('../config.ini'):
+        config_path = '../config.ini'
+    else:
+        log_error("config.ini file does not exist in the working directory or its parent directory. Exiting.")
+        sys.exit()
+
+    return config_path
+
+
+def validate_and_get_warc_gz_archives_directory(parsed_warc_gz_archives_directory: str):
     """Validates that the directory containing the warc.gz archives exists and has .gz files present."""
-    if not os.path.exists(warc_gz_archives_directory):
-        log_error(f"Directory containing the warc.gz archives to search does not exist: {warc_gz_archives_directory}")
+    if not os.path.exists(parsed_warc_gz_archives_directory):
+        log_error(f"Directory containing the warc.gz archives to search does not exist: {parsed_warc_gz_archives_directory}. Exiting.")
         sys.exit()
 
-    if not glob.glob(warc_gz_archives_directory + '/*.gz'):
-        log_error(f"Directory that should contain the .gz archives to search does not contain any: {warc_gz_archives_directory}")
+    if not glob.glob(parsed_warc_gz_archives_directory + '/*.gz'):
+        log_error(f"Directory that should contain the .gz archives to search does not contain any: {parsed_warc_gz_archives_directory}. Exiting.")
         sys.exit()
 
+    return parsed_warc_gz_archives_directory
 
-def validate_search_regex_definitions_directory(search_regex_definitions_directory: str):
+
+def validate_and_get_search_regex_definitions_directory(parsed_search_regex_definitions_directory: str):
     """Validates that the directory containing the regex definition text files exists and has .txt files present."""
-    if not os.path.exists(search_regex_definitions_directory):
-        log_error(f"Directory containing the regex definition .txt files to search with does not exist: {search_regex_definitions_directory}")
+    if not os.path.exists(parsed_search_regex_definitions_directory):
+        log_error(f"Directory containing the regex definition .txt files to search with does not exist: {parsed_search_regex_definitions_directory}. Exiting.")
         sys.exit()
 
-    if not glob.glob(search_regex_definitions_directory + '/*.txt'):
-        log_error(f"Directory that should contain the regex definition .txt files to search with does not contain any: {search_regex_definitions_directory}")
+    if not glob.glob(parsed_search_regex_definitions_directory + '/*.txt'):
+        log_error(f"Directory that should contain the regex definition .txt files to search with does not contain any: {parsed_search_regex_definitions_directory}. Exiting.")
         sys.exit()
 
+    return parsed_search_regex_definitions_directory
 
-def validate_results_output_directory(results_output_directory: str):
+
+def validate_and_get_results_output_directory(parsed_results_output_directory: str):
     """Validates that the directory to output the search results to exists."""
-    if not os.path.exists(results_output_directory):
-        log_error(f"Directory to output the search results to does not exist: {results_output_directory}")
+    if not os.path.exists(parsed_results_output_directory):
+        log_error(f"Directory to output the search results to does not exist: {parsed_results_output_directory}. Exiting.")
         sys.exit()
+    
+    return parsed_results_output_directory
 
 
-def validate_and_get_max_concurrent_search_processes(parsed_config_value: str) -> int:
+def validate_and_get_max_concurrent_search_processes(parsed_max_concurrent_search_processes: str) -> int:
     """
     Validates the maximum number of concurrent search processes config.ini value. 
     If invalid, sets it to the maximum logical processors available.
@@ -44,8 +62,8 @@ def validate_and_get_max_concurrent_search_processes(parsed_config_value: str) -
 
     try:
         max_concurrent_search_processes = (
-            total_logical_processors if parsed_config_value == "none" 
-            else int(parsed_config_value)
+            total_logical_processors if parsed_max_concurrent_search_processes == "none" 
+            else int(parsed_max_concurrent_search_processes)
         )
 
         if max_concurrent_search_processes <= 0 or max_concurrent_search_processes > total_logical_processors:
@@ -53,7 +71,7 @@ def validate_and_get_max_concurrent_search_processes(parsed_config_value: str) -
 
     except ValueError:
         log_warning(
-            f"Invalid value for MAX_CONCURRENT_SEARCH_PROCESSES in config.ini: {parsed_config_value}. "
+            f"Invalid value for MAX_CONCURRENT_SEARCH_PROCESSES in config.ini: {parsed_max_concurrent_search_processes}. "
             f"Setting number of concurrent search processes to maximum logical processors available on the PC."
         )
         max_concurrent_search_processes = total_logical_processors
@@ -61,7 +79,7 @@ def validate_and_get_max_concurrent_search_processes(parsed_config_value: str) -
     return max_concurrent_search_processes
 
 
-def validate_and_get_max_ram_usage(parsed_config_value: str) -> int:
+def validate_and_get_max_ram_usage_bytes(parsed_max_ram_usage_bytes: str) -> int:
     """
     Validates the maximum RAM usage config.ini value for the program execution in bytes.
     If invalid, sets it to the total amount of RAM available on the machine.
@@ -70,8 +88,8 @@ def validate_and_get_max_ram_usage(parsed_config_value: str) -> int:
 
     try:
         max_ram_usage_in_bytes = (
-            total_machine_ram_in_bytes if parsed_config_value == "none" 
-            else int(parsed_config_value)
+            total_machine_ram_in_bytes if parsed_max_ram_usage_bytes == "none" 
+            else int(parsed_max_ram_usage_bytes)
         )
 
         if max_ram_usage_in_bytes <= 0 or max_ram_usage_in_bytes > total_machine_ram_in_bytes:
@@ -79,7 +97,7 @@ def validate_and_get_max_ram_usage(parsed_config_value: str) -> int:
 
     except ValueError:
         log_warning(
-            f"Invalid value for MAX_RAM_USAGE_BYTES in config.ini: {parsed_config_value}. "
+            f"Invalid value for MAX_RAM_USAGE_BYTES in config.ini: {parsed_max_ram_usage_bytes}. "
             f"Setting maximum RAM usage to the total amount of RAM available on the PC."
         )
         max_ram_usage_in_bytes = total_machine_ram_in_bytes
